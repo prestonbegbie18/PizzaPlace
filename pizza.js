92736221a5b104423f737b2647fd9ed1db568070
Kevin Buffardi <kbuffardi@csuchico.edu>
2016-11-02 15:49:17 -0700

    Checkout added

--- pizza.js
+++ pizza.js
@@ -50,9 +50,27 @@ function order()
   setCookie("custom",customPizzas);
 }
 
+// put the info in:
+/*
+<p id="cheeseCount"></p>
+<p id="meatCount"></p>
+<p id="vegCount"></p>
+<p id="customCount"></p>
+*/
 function checkoutOrders()
 {
-  window.alert("Automatically loaded!");
+  var numCheese = getCookie("cheese");
+  var numMeat = getCookie("meatlover");
+  var numVeg = getCookie("veggie");
+  var numCustom = getCookie("custom");
+
+  document.getElementById("cheeseCount").innerHTML = numCheese + " Cheese pizzas";
+  document.getElementById("meatCount").innerHTML = numMeat + " Meat Lovers pizzas";
+  document.getElementById("vegCount").innerHTML = numVeg + " Veggie pizzas";
+  document.getElementById("customCount").innerHTML = numCustom + " Custom pizzas";
+
+
+
 }
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
