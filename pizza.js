99152561ef2e4b443f076810ce2f51c3bbb7833c
Unknown <pbegbie@mail.csuchico.edu>
2017-03-22 09:43:44 -0700

    What we have done so far

--- pizza.js
+++ pizza.js
@@ -1,7 +1,38 @@
-window.onload = main;
+window.onload = showValues;
 
-function main()
+function showValues()
 {
-  console.log("Hello World!");
+  var veg = document.getElementById("checkVeggies");
+  //veg.defaultChecked = true;
+  var name = document.getElementById("orderName");
+  //name.value = "Preston";
+
+  console.log("Finished Running JavaScript");
+
 }
 
+function order(){
+
+  var name = document.getElementById("orderName");
+//get the status of the check box
+  var veg = document.getElementById("checkVeggies").checked;
+  var pep = document.getElementById("checkPepperoni").checked;
+  var sausage = document.getElementById("checkSausage").checked;
+
+  if(veg == true && pep == true && sausage == true)
+  {
+    console.log("Order received by " + name.value);
+    console.log("Order is a supreme pizza");
+  }
+  else if (pep == true && sausage == true)
+  {
+    console.log("Order received by " + name.value);
+    console.log("Order is a meatlovers pizza");
+  }
+
+  else if(veg == false && pep == false && sausage == false)
+  {
+    console.log("Order received by " + name.value);
+    console.log("Order is a cheese pizza");
+  }
+}
